ed74428d1a1167962ea6125e890e3432a47b6801
paing_soe_thaw <paingsoethaw@gmail.com>
2018-05-14 15:41:49 +0800

    React Elements: JSX, Babel, Simple

--- src/index.js
+++ src/index.js
@@ -4,23 +4,52 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// const name = 'Josh Perez';
+
+function greetingText(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}</h1>;
+  }
+  return <h1>Hello, Stranger</h1>
+}
 
 function formatName(user) {
   return user.firstName + " " + user.lastName;
+}
+
+function printElement(element) {
+  return element;
 }
 
 const user = {
   firstName: "David",
   lastName: "Slim"
 }
+// const user = "";
 
-const greetingText = (
-  <h1>Hello, {formatName(user)}</h1>
+/////****React elements - JSX****////
+// const element = (
+//   <h1 className="greeting">
+//     Hello, world!
+//   </h1>
+// );
+
+/////****React elements - Babel****////
+const element = React.createElement(
+  'h1',
+  { className: 'greeting' },
+  'Hello, world!'
 );
 
+/////****React elements - Simplified Structure****////
+// const element = {
+//   type: 'h1',
+//   props: {
+//     className: 'greeting',
+//     children: 'Hello, World!'
+//   }
+// };
 
-ReactDOM.render(greetingText,
+ReactDOM.render(printElement(element),
   document.getElementById('root')
 );
 
