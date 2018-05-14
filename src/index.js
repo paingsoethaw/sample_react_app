69024881b973ffe74cdac09c429fb9fbfc67d925
paing_soe_thaw <paingsoethaw@gmail.com>
2018-05-14 11:58:06 +0800

    code clean

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class ShoppingList extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>Shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>              
+            </div>
+        )
+    }
+}
+
+
+
 class Square extends React.Component {
     render() {
       return (
@@ -51,7 +68,7 @@ class Square extends React.Component {
             <Board />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{renderSquare(i)}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
