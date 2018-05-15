56d6ba2fe410ceebfad73bf82be9e10cc0a81d38
paing_soe_thaw <paingsoethaw@gmail.com>
2018-05-15 12:25:48 +0800

    Testing array in jsx element

--- src/CompoNProps/ExtractingComponent.js
+++ src/CompoNProps/ExtractingComponent.js
@@ -1,48 +1,81 @@
 import React, { Component } from 'react';
 
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+// function Avatar(props) {
+//     return (
+//         <img
+//             className="Avatar"
+//             src={props.user.avatarUrl}
+//             alt={props.user.name}
+//         />
+//     );
+// }
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            {/* <Avatar user={props.user} /> */}
+            <img
+                className="Avatar"
+                src={props.user.avatarUrl}
+                alt={props.user.name}
+            />
+
+            <div className="UserInfo-name">{props.user.name}</div>
+        </div>
+    );
+}
+
 function Comment(props) {
     return (
         <div className="Comment">
-            <div className="UserInfo">
-                <img
-                    className="Avatar"
-                    src={props.author.avatarUrl}
-                    alt={props.author.name}
-                />
-                <div className="UserInfo-name">{props.author.name}</div>
-            </div>
+            <UserInfo user={props.author} />
             <div className="Comment-text">{props.text}</div>
             <div className="Comment-date">
-                {props.date.toLocaleDateString()}
+                {formatDate(props.date)}
             </div>
         </div>
     );
 }
 
-const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'Mrs. Kitty',
-        avatarUrl: 'http://placekitten.com/g/64/64',
+const comments = [
+    {
+        date: new Date(),
+        text: 'I hope you enjoy learning React!',
+        author: {
+            name: 'Mrs. Kitty',
+            avatarUrl: 'http://placekitten.com/g/64/64',
+        },
     },
-};
+    {
+        date: new Date(),
+        text: 'React is libary for building UI!',
+        author: {
+            name: 'Mrs. React',
+            avatarUrl: 'http://placekitten.com/g/64/64',
+        },
+    }
+];
 
-function ShowComment() {
-    return (
-        <div>
-            <Comment
-                date={comment.date}
-                text={comment.text}
-                author={comment.author}
-            />
-        </div>
-    );
-}
+
 class ExtractingComponent extends Component {
     render() {
         return (
-            ShowComment()
+            <div>
+                <Comment
+                    date={comments[0].date}
+                    text={comments[0].text}
+                    author={comments[0].author}
+                />
+                <Comment
+                    date={comments[1].date}
+                    text={comments[1].text}
+                    author={comments[1].author}
+                />
+            </div>
         );
     }
 }
