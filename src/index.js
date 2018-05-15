6042adbb0b70f5b30e487fcd95950c2fbada4c8f
paing_soe_thaw <paingsoethaw@gmail.com>
2018-05-15 11:59:37 +0800

    Components and Props

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import CompoNProps from './CompoNProps/CompoNProps';
-import ComposingCompo from './CompoNProps/ComposingCompo';
+// import ComposingCompo from './CompoNProps/ComposingCompo';
+import ExtractingComponent from './CompoNProps/ExtractingComponent';
 
 import registerServiceWorker from './registerServiceWorker';
 
 registerServiceWorker();
 
-ReactDOM.render(<ComposingCompo />, document.getElementById('root'));
+ReactDOM.render(<ExtractingComponent />, document.getElementById('root'));
