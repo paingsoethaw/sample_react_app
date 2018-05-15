acaf0a84c6127b7e83c1762b8c8568a6612a145e
paing_soe_thaw <paingsoethaw@gmail.com>
2018-05-15 12:04:55 +0800

    Components without extracting

--- src/CompoNProps/ExtractingComponent.js
+++ src/CompoNProps/ExtractingComponent.js
@@ -1,35 +1,19 @@
 import React, { Component } from 'react';
 
-function formatDate(date) {
-    return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-    return (
-        <img
-            className="Avatar"
-            src={props.user.avatarUrl}
-            alt={props.user.name}
-        />
-    );
-}
-
-function UserInfo(props) {
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div className="UserInfo-name">{props.user.name}</div>
-        </div>
-    );
-}
-
 function Comment(props) {
     return (
         <div className="Comment">
-            <UserInfo user={props.author} />
+            <div className="UserInfo">
+                <img
+                    className="Avatar"
+                    src={props.author.avatarUrl}
+                    alt={props.author.name}
+                />
+                <div className="UserInfo-name">{props.author.name}</div>
+            </div>
             <div className="Comment-text">{props.text}</div>
             <div className="Comment-date">
-                {formatDate(props.date)}
+                {props.date.toLocaleDateString()}
             </div>
         </div>
     );
@@ -46,11 +30,13 @@ const comment = {
 
 function ShowComment() {
     return (
-        <Comment
-            date={comment.date}
-            text={comment.text}
-            author={comment.author}
-        />
+        <div>
+            <Comment
+                date={comment.date}
+                text={comment.text}
+                author={comment.author}
+            />
+        </div>
     );
 }
 class ExtractingComponent extends Component {
